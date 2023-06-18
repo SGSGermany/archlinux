e35c158d355c777cffcd0c665244143a9233866e
Daniel Rudolf <github.com@daniel-rudolf.de>
2023-06-18 10:50:27 +0200

    Update build scripts to use latest CI tool standards

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -11,7 +11,7 @@
 # License-Filename: LICENSE
 
 set -eu -o pipefail
-export LC_ALL=C
+export LC_ALL=C.UTF-8
 
 [ -v CI_TOOLS ] && [ "$CI_TOOLS" == "SGSGermany" ] \
     || { echo "Invalid build environment: Environment variable 'CI_TOOLS' not set or invalid" >&2; exit 1; }
@@ -20,6 +20,7 @@ export LC_ALL=C
     || { echo "Invalid build environment: Environment variable 'CI_TOOLS_PATH' not set or invalid" >&2; exit 1; }
 
 source "$CI_TOOLS_PATH/helper/common.sh.inc"
+source "$CI_TOOLS_PATH/helper/common-traps.sh.inc"
 
 BUILD_DIR="$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 source "$BUILD_DIR/container.env"
@@ -36,6 +37,8 @@ if [ -z "$CONTAINER" ]; then
     echo "build"
     exit
 fi
+
+trap_exit buildah rm "$CONTAINER"
 
 # run `pacman -Sy` and `pacman -Qu` to check for package updates
 cmd buildah run "$CONTAINER" -- \
